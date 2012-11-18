16cda3865b5ebbbce7e2e83ec34fd66716180a85
Michael DiScala <mike.discala@gmail.com>
2012-11-18 21:06:28 +0000

    added ordering constraint to prevent passenger gem from installing before ruby itself

--- manifests/passenger/gem.pp
+++ manifests/passenger/gem.pp
@@ -2,6 +2,8 @@ class rvm::passenger::gem($ruby_version, $version) {
   rvm_gem {
     "passenger":
       ensure       => $version,
-      ruby_version => $ruby_version;
+      ruby_version => $ruby_version,
+      require      => Rvm_system_ruby[$ruby_version];
   }
+
 }
